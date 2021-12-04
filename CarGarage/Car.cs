08f8a48179121e52fc681b6ff8f61d182fead2fb
RomanShmi <90277345+RomanShmi@users.noreply.github.com>
2021-12-03 23:28:10 -0500

    Initial commit

---
+++ CarGarage/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGarage
+{
+    public class Car
+    {
+        public string Make;
+        public string Model;
+        public int Speed { get; set; }
+        public int FuelLevel { get; set; }
+        public bool AutoBrakeActive { get; set; }
+
+        public void SetMake(String make)
+        {
+            this.Make = make;
+            Console.WriteLine("Make set to: " + this.Make);
+        }
+        public void SetModel(String model)
+        {
+            this.Model = model;
+            Console.WriteLine("Model set to: " + this.Model);
+        }
+        public int GetSpeed()
+        {
+            return this.Speed;
+        }
+        public bool IsSpeeding()
+        {
+            return this.Speed > 70;
+        }
+        public void Accelerate()
+        {
+            
+           
+        if (this.Make == "Ferrari")
+            {
+            if (this.Speed + 20 <= 100)
+                {
+                this.Speed += 20;
+                } 
+            }
+        else
+            {
+            if (this.Speed + 10 <= 100)
+                {
+                this.Speed += 10;
+                } 
+            
+            }
+             
+        }
+        public void Brake()
+        {
+            if (!AutoBrakeActive)
+            {
+                if (this.Speed - 7 < 0)
+                {
+                    this.Speed = 0;
+                }
+                else
+                {
+                    this.Speed -= 7;
+                }
+            }
+            else
+            {
+                this.Speed = Convert.ToInt32(Math.Round(Convert.ToDouble(this.Speed) * .5));
+                    
+            }
+        }
+    }
+}
---
+++ CarGarage/CarGarage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarGarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wecome to Car Garage!");
+            // this is just me trying things out in a sort of sandbox
+            Car myCar = new Car();
+            myCar.SetMake("Ford");
+            myCar.SetModel("Contour");
+            myCar.Speed = 1;
+            myCar.AutoBrakeActive = true;
+            myCar.Brake();
+            Console.WriteLine(myCar.Speed);
+            
+
+        }
+    }
+}
